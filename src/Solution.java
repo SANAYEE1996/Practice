7ecd701efd18cda50ba9af21bfd70640e3ee7db6
SANAYEE1996 <dudtkd0219@gmail.com>
2021-05-07 17:26:41 +0900

    210507

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,27 @@
-import java.util.Arrays;
-
 public class Solution {
     public static void main(String args[]) {
-    	int N = 5;
-    	int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
-    	int[] answer = new int[N];
+    	int[][] triangle = {     {7},
+    						    {3,8},
+    						   {8,1,0},
+    						  {2,7,4,4},
+    						 {4,5,2,6,5}
+    						};
+    	int answer = 0;
     	
-    	Arrays.sort(stages);
-    	
-    	
+    	dp(triangle);
     	
         System.out.println("the answer is " +answer);
+    }
+    
+    private static int dp(int[][] arr) {
+    	int n = arr.length;
+    	for(int i = 1; i< arr.length; i++) {
+    		for(int j = 0; j < arr[i].length; j++) {
+    			if(j <1) arr[i][j] += arr[i-1][j];
+    			else arr[i][j] += arr[i-1][j-1];
+    		}
+    	}
+    	
+    	return n;
     }
 }
