f7b9208306a64d385dd18b140a341a5a0aa2097b
SANAYEE1996 <dudtkd0219@gmail.com>
2021-05-10 13:09:45 +0900

    zz

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,17 @@
-public class Solution {
-    public static void main(String args[]) {
-    	int[][] triangle = {     {7},
-    						    {3,8},
-    						   {8,1,0},
-    						  {2,7,4,4},
-    						 {4,5,2,6,5}
-    						};
-    	int answer = 0;
-    	
-    	dp(triangle);
-    	
-        System.out.println("the answer is " +answer);
-    }
-    
-    private static int dp(int[][] arr) {
-    	int n = arr.length;
-    	for(int i = 1; i< arr.length; i++) {
-    		for(int j = 0; j < arr[i].length; j++) {
-    			if(j <1) arr[i][j] += arr[i-1][j];
-    			else arr[i][j] += arr[i-1][j-1];
-    		}
-    	}
-    	
-    	return n;
+class Solution {
+    public int solution(int[] arr) {
+        int answer = -1;
+        int div = 2;
+        for(int i = 0; i<arr.length; i++) {
+        	int a = arr[i];
+        	for(int j = 0; j < arr.length; j++) {
+        		if(arr[j] % div == 0) {
+        			break;
+        		}
+        	}
+        }
+        
+        
+        return answer;
     }
 }
