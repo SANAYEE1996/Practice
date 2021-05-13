c3aad40e303912c893917ab5bc1374053054a316
SANAYEE1996 <dudtkd0219@gmail.com>
2021-05-14 08:53:17 +0900

    hmm

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
- 
+	
 	public static void main(String[] args) {
+		
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		
 		int a = sc.nextInt();
-		System.out.println(fibo(a));
-		
-		
-	}
-	
-	private static int fibo(int n) {
-		
-		if(n == 1) return 0;
-		else if(n == 2) return 1;
-		else {
-			for(int i = 0; i< n; i++) {
-				
-			}
-			return fibo(n);
+		String[] str = new String[a];
+		for(int i = 0; i < str.length; i++) {
+			str[i] = sc.next();
+		}
+		Arrays.sort(str);
+		for(int i = 0; i < str.length;i++) {
+			System.out.println(str[i]);
 		}
 	}
- 
+	
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,53 @@
+
 class Solution {
-    public int solution(int[] arr) {
-        int answer = -1;
-        int div = 2;
-        for(int i = 0; i<arr.length; i++) {
-        	int a = arr[i];
-        	for(int j = 0; j < arr.length; j++) {
-        		if(arr[j] % div == 0) {
-        			break;
-        		}
-        	}
-        }
-        
-        
-        return answer;
-    }
+	public static void main(String args[]) {
+		String dartResult = "1D2S3T*";
+		int answer = 0;
+		answer = score(dartResult,answer);
+		
+		
+		System.out.println("the answer is "+answer);
+	}
+	
+	private static int score(String s,int answer) {
+		int a = 0;
+		char c = 0;
+		char d = 0;
+		int b = 0;
+		for(int i = 0; i < s.length(); i++) {
+			c = s.charAt(i);
+			if(i < s.length()-1) d = s.charAt(i+1);
+			if(Character.isDigit(c)){
+				if(Character.getNumericValue(c) == 1 && Character.getNumericValue(d) == 0) {
+					a = 10;
+					i++;
+					continue;
+				}
+				a = Character.getNumericValue(c);
+			}
+			else{
+				if(c == 'S') {
+					answer += a;
+				}
+				else if(c == 'D') {
+					a = (int) Math.pow(a, 2);
+					answer += a;
+				}
+				else if(c == 'T') {
+					a = (int) Math.pow(a, 3);
+					answer += a;
+				}
+				else if(c == '*') {
+					answer = a*2 + b*a;
+					b = 0;
+				}
+				else if(c == '#') {
+					answer -= a*2;
+				}
+			}
+		}
+		
+		return answer;
+	}
+	
 }
